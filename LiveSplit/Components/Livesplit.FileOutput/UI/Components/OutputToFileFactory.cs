20d90ddfd538d9f347a6ab5387e7e6a367189a22
lotsofs <oeneboy@hotmail.com>
2021-07-12 01:36:03 +0200

    Add FileOutput because the other one didn't work

---
+++ LiveSplit/Components/Livesplit.FileOutput/UI/Components/OutputToFileFactory.cs
@@ -0,0 +1,24 @@
+﻿using LiveSplit.Model;
+using System;
+
+namespace LiveSplit.UI.Components
+{
+    public class OutputToFileFactory : IComponentFactory
+    {
+        public string ComponentName => "Output To File";
+
+        public string Description => "Output information to a .txt file";
+
+        public ComponentCategory Category => ComponentCategory.Other;
+
+        public IComponent Create(LiveSplitState state) => new OutputToFile();
+
+        public string UpdateName => ComponentName;
+
+        public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.OutputToFile.xml";
+
+        public string UpdateURL => "http://livesplit.org/update/";
+
+        public Version Version => Version.Parse("0.1.1");
+    }
+}
